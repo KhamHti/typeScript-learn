5cf003694321b26cdf2831a1a186c3dc49226374
Khun Kyaw Kham Hti <khamhti@agga.io>
2023-10-24 00:13:57 +0630

    object

---
+++ object/myObject.js
@@ -0,0 +1,16 @@
+var tester = {
+    name: "kham",
+    email: "kham@kham.com",
+    isError: false,
+    age: 25,
+};
+function createTester(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createTester({ name: 'thu thu', isPaid: true });
+function createCourse() {
+    return { name: 'reactjs', price: 399 };
+}
+var createToken = function () {
+    return { name: 'guestAuth', id: 123123123, age: 30 };
+};
---
+++ object/myObject.ts
@@ -0,0 +1,18 @@
+const tester = {
+    name: "kham",
+    email: "kham@kham.com",
+    isError: false,
+    age: 25,
+}
+
+function createTester({name: string, isPaid: boolean}){}
+
+createTester({name: 'thu thu', isPaid: true})
+
+function createCourse():{name: string,price: number}{
+    return {name: 'reactjs', price: 399}
+}
+
+const createToken = ():{name: string, id: number, age: number} => {
+    return {name: 'guestAuth', id: 123123123, age: 30}
+}
