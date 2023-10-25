2d91d1ef23727e2512a778f546486cbbdfa186d8
Khun Kyaw Kham Hti <khamhti@agga.io>
2023-10-25 23:11:53 +0630

    arrays

---
+++ myArrays/myArrays.js
@@ -0,0 +1,6 @@
+var superHeroes = [];
+superHeroes.push('spiderman');
+var asdf = [];
+asdf.push(1);
+var allUser = [];
+allUser.push({ name: 'asdf', isActive: true });
---
+++ myArrays/myArrays.ts
@@ -0,0 +1,22 @@
+const superHeroes: string[] = [];
+
+superHeroes.push('spiderman');
+
+const asdf: number[] = [];
+
+asdf.push(1)
+
+type user = {
+    name: string,
+    isActive: boolean 
+}
+ 
+const allUser: user[] = [];
+
+allUser.push({name: 'asdf', isActive: true})
+
+
+const MLModels: number[][] = [
+    [12,12,12,1],
+    []
+]
